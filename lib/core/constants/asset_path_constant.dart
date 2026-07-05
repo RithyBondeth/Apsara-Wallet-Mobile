445c709be4b01430febeeec8c2e27af19ffddb26
RITHY Bondeth <rithybondeth999@gmail.com>
2026-07-05 20:39:59 +0700

    feat: register asset directories and implement AssetPathConstant for image resources

---
+++ lib/core/constants/asset_path_constant.dart
@@ -0,0 +1,22 @@
+class AssetPathConstant {
+  AssetPathConstant._();
+
+  static const String splashLogo = 'assets/images/splash_logo.png';
+  static const String onBoarding1 = 'assets/images/onboarding_1.png';
+  static const String onBoarding2 = 'assets/images/onboarding_2.png';
+  static const String onBoarding3 = 'assets/images/onboarding_3.png';
+
+  static const String loginBackground = 'assets/images/login_background.png';
+  static const String registerBackground =
+      'assets/images/register_background.png';
+  static const String forgotPasswordBackground =
+      'assets/images/forgot_password_background.png';
+  static const String resetPasswordBackground =
+      'assets/images/reset_password_background.png';
+  static const String pinSetupBackground =
+      'assets/images/pin_setup_background.png';
+  static const String pinLoginBackground =
+      'assets/images/pin_login_background.png';
+  static const String biometricBackground =
+      'assets/images/biometric_background.png';
+}
