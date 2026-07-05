3de1cfa1ae2c7b20f14aa68b1938b686afb9b350
RITHY Bondeth <rithybondeth999@gmail.com>
2026-07-05 20:43:13 +0700

    feat: implement system-based light and dark themes and add devtools configuration

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:apsara_wallet_mobile/core/configs/config_service.dart';
+import 'package:apsara_wallet_mobile/core/themes/app_theme.dart';
 import 'package:apsara_wallet_mobile/features/auth/presentation/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,7 +12,9 @@ class MyApp extends ConsumerWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: AppConfigService.appName,
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
       home: const LoginScreen(),
     );
   }
